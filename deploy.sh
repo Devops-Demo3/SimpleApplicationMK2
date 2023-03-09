7da2c8c2c28d4e8ee18c5c42acd7c3a2d0ad06c9
bregor77 <bregor77@gmail.com>
2023-03-09 13:57:02 +0000

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ sudo npm install -g pm2
 pm2 stop example_app
 cd SimpleApplicationMK2/
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 # sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 # pm2 start ./bin/www --name example_app --env=production
 pm2 start ./bin/www --name example_app
